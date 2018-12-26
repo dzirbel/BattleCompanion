0fbaa1c411bb6cec3733852cc79a69dd9b5b9b6c
Dominic Zirbel <dzirbel@thumbtack.com>
2018-12-26 16:20:41 -0500

    Fix bug with submarine opening fire

--- app/src/main/java/com/github/dzirbel/battlecompanion/core/UnitType.kt
+++ app/src/main/java/com/github/dzirbel/battlecompanion/core/UnitType.kt
@@ -83,7 +83,7 @@ enum class UnitType(
         return when (this) {
             ANTIAIRCRAFT_GUN -> true
             BOMBARDING_BATTLESHIP -> true
-            SUBMARINE -> enemies.units.any { it.key == DESTROYER }
+            SUBMARINE -> enemies.units.none { it.key == DESTROYER }
             else -> false
         }
     }
