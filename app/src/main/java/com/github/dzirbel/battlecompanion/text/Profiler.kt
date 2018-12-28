0f288f0e66658d5f4a76b8eab5e72e5ba1447f1e
Dominic Zirbel <dzirbel@thumbtack.com>
2018-12-28 00:56:56 -0500

    Save Profiler runs as text files

--- app/src/main/java/com/github/dzirbel/battlecompanion/text/Profiler.kt
+++ app/src/main/java/com/github/dzirbel/battlecompanion/text/Profiler.kt
@@ -4,6 +4,10 @@ import com.github.dzirbel.battlecompanion.core.Army
 import com.github.dzirbel.battlecompanion.core.Board
 import com.github.dzirbel.battlecompanion.core.CasualtyPicker
 import com.github.dzirbel.battlecompanion.core.UnitType
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 import kotlin.random.Random
 
@@ -47,11 +51,28 @@ private val armies = mapOf(
 )
 
 private val rand = Random(42)
-private const val N = 1_000_000
+private val log = StringBuilder()
 
-// TODO output profiling results to a text file
+private const val N = 1_000_000
+private const val printArmies = true
+private const val saveLog = true
+private val runFilenameDateFormat = SimpleDateFormat("yyyy-MM-dd_kk-mm-ss", Locale.US)
+
 fun main() {
     val start = System.nanoTime()
+
+    if (printArmies) {
+        logln("Armies:")
+        armies.forEach { name, army ->
+            logln("  $name")
+            army.forEach { (unitType, count) ->
+                logln("    $count of ${unitType.prettyName}")
+            }
+            logln()
+        }
+        logln()
+    }
+
     val scores = mutableListOf<Int>()
     val armyScores = mutableMapOf<String, List<Int>>()
 
@@ -67,23 +88,28 @@ fun main() {
             .average()
             .toInt()
 
-        println("Score: ${averageScore.format()}")
-        println()
+        logln("Score: ${averageScore.format()}")
+        logln()
         scores.add(averageScore)
     }
 
     val total = scores.average().toInt()
     val totalDuration = TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - start)
-    println()
-    println("Done in $totalDuration seconds!")
-    println("Overall profiling score: ${total.format()} (lower is better)")
+
+    logln()
+    logln("Done in $totalDuration seconds!")
+    logln("Overall profiling score: ${total.format()} (lower is better)")
     armies.toSortedMap().forEach { (name, _) ->
-        println("  ${name.capitalize()} battle : ${armyScores[name]?.average()?.toInt()}")
+        logln("  ${name.capitalize()} battle : ${armyScores[name]?.average()?.toInt()}")
+    }
+
+    if (saveLog) {
+        saveLog()
     }
 }
 
 private fun profileBattle(units: Map<UnitType, Int>, name: String): Long {
-    print("Profiling $name battle with ${N.format()} runs...")
+    log("Profiling $name battle with ${N.format()} runs...")
     val start = System.nanoTime()
 
     val startingBoard = Board(
@@ -102,7 +128,28 @@ private fun profileBattle(units: Map<UnitType, Int>, name: String): Long {
         }
     }
     val duration = System.nanoTime() - start
-    println(" done in ${TimeUnit.NANOSECONDS.toMillis(duration)}ms.")
+    logln(" done in ${TimeUnit.NANOSECONDS.toMillis(duration)}ms.")
 
     return duration / N
+}
+
+private fun log(message: String = "") {
+    print(message)
+    log.append(message)
+}
+
+private fun logln(message: String = "") {
+    println(message)
+    log.appendln(message)
+}
+
+private fun saveLog() {
+    val dir = File("profiler-runs")
+    dir.mkdir()
+
+    val logFile = dir.resolve("run_${runFilenameDateFormat.format(Date())}.txt")
+    logFile.writeText(log.toString())
+
+    println()
+    println("Run saved as ${logFile.relativeTo(File("")).path}")
 }
